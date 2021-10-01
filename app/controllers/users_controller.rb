10361bd6290e8322d6b25295de8c2902ed13b364
bruno-costanzo <dev.bcostanzo@gmail.com>
2021-09-30 23:59:13 -0300

    Almost full CRUD maded for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,41 @@
 
 # Users Controller
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
-  def create; end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
